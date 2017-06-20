d2aa77ae7bd09f8cbafe4bae1aed9e83aa4aa2c6
ivaylospasov <ivaylo.pif@gmail.com>
2017-06-20 06:47:13 +0300

    Test lower() on save on original text.

--- correct/models.py
+++ correct/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class Original(models.Model):
     title = models.CharField(max_length=200)
     original_text = models.TextField()
+    corrected_text = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.corrected_text = self.original_text.lower()
+        super(Original, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
