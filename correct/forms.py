fe96640fdbdb7b39fe51579dd6a69b2d329f517e
ivaylospasov <ivaylo.pif@gmail.com>
2017-06-21 21:17:25 +0300

    Finish the form, not connected to model

--- correct/forms.py
+++ correct/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.core import validators
 
 class NewsInput(forms.Form):
     title = forms.CharField()
     original_text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,
+                                widget=forms.HiddenInput,
+                                validators=[validators.MaxLengthValidator(0)])
